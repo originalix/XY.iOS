fa41a5ee254c5a1ecb488f077b482500aabffaf9
Lix <xiao.liunit@gmail.com>
2017-06-30 12:02:55 +0800

    Initial commit

---
+++ XY.iOS/XY.iOSTests/XY_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  XY_iOSTests.swift
+//  XY.iOSTests
+//
+//  Created by Lix on 2017/6/30.
+//  Copyright © 2017年 Lix. All rights reserved.
+//
+
+import XCTest
+@testable import XY_iOS
+
+class XY_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
