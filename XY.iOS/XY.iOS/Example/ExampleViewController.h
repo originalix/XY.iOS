170f7fe1854bcaf205ccd77c920fc22a06dbcb42
Lix <xiao.liunit@gmail.com>
2017-06-30 12:22:43 +0800

    [ExampleViewController]: 增加测试类

---
+++ XY.iOS/XY.iOS/Example/ExampleViewController.h
@@ -0,0 +1,13 @@
+//
+//  ExampleViewController.h
+//  XY.iOS
+//
+//  Created by Lix on 2017/6/30.
+//  Copyright © 2017年 Lix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExampleViewController : UIViewController
+
+@end
---
+++ XY.iOS/XY.iOS/Example/ExampleViewController.m
@@ -0,0 +1,29 @@
+//
+//  ExampleViewController.m
+//  XY.iOS
+//
+//  Created by Lix on 2017/6/30.
+//  Copyright © 2017年 Lix. All rights reserved.
+//
+
+#import "ExampleViewController.h"
+
+@interface ExampleViewController ()
+
+@end
+
+@implementation ExampleViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+- (void)testDB {
+    
+}
+
+@end
