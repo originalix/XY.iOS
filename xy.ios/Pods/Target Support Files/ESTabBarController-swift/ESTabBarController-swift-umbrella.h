c410b5694db2adc10d16a874efbfc1f74214eb33
Lix <xiao.liunit@gmail.com>
2017-06-30 12:00:16 +0800

    [清除原项目做他用]

--- xy.ios/Pods/Target Support Files/ESTabBarController-swift/ESTabBarController-swift-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double ESTabBarController_swiftVersionNumber;
-FOUNDATION_EXPORT const unsigned char ESTabBarController_swiftVersionString[];
-
