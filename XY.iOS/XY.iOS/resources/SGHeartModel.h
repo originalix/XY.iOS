1e45080715b5b3d9e45499cf7a11165076a687c6
Lix <xiao.liunit@gmail.com>
2017-06-30 12:15:17 +0800

    [Objc预编译文件]

---
+++ XY.iOS/XY.iOS/resources/SGHeartModel.h
@@ -0,0 +1,42 @@
+//
+//  SGHeartModel.h
+//  SGHome
+//
+//  Created by Lix on 2017/6/30.
+//  Copyright © 2017年 State Grid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SGHeartModel : NSObject
+
+@end
+
+@interface SGHeartTable : NSObject
+
+@property (nonatomic, assign) NSInteger _id;
+@property (nonatomic, copy) NSString *date;
+@property (nonatomic, copy) NSString *created_at;
+@property (nonatomic, copy) NSString *updated_at;
+
+@end
+
+@interface SGHeartOriginTable : NSObject
+
+@property (nonatomic, copy) NSString *srcData;
+@property (nonatomic, copy) NSString *date;
+@property (nonatomic, assign) NSInteger timestamp;
+@property (nonatomic, copy) NSString *created_at;
+@property (nonatomic, copy) NSString *updated_at;
+
+@end
+
+@interface SGHeartDetailsTable : NSObject
+
+@property (nonatomic, assign) NSInteger dataID;
+@property (nonatomic, assign) NSInteger heart;
+@property (nonatomic, copy) NSString *time;
+@property (nonatomic, copy) NSString *timestamp;
+@property (nonatomic, copy) NSString *created_at;
+
+@end
---
+++ XY.iOS/XY.iOS/resources/SGHeartModel.m
@@ -0,0 +1,25 @@
+//
+//  SGHeartModel.m
+//  SGHome
+//
+//  Created by Lix on 2017/6/30.
+//  Copyright © 2017年 State Grid. All rights reserved.
+//
+
+#import "SGHeartModel.h"
+
+@implementation SGHeartModel
+
+@end
+
+@implementation SGHeartTable
+
+@end
+
+@implementation SGHeartOriginTable
+
+@end
+
+@implementation SGHeartDetailsTable
+
+@end
