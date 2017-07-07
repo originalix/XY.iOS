8a3f1b115fdedcbfef9d9e0bdc1d1838d6307a54
Lix <xiao.liunit@gmail.com>
2017-07-07 16:47:40 +0800

    Camera Stroyboard

--- XY.iOS/XY.iOS/AppDelegate.swift
+++ XY.iOS/XY.iOS/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
         self.window?.rootViewController = UINavigationController.init(rootViewController: ExampleViewController())
         self.window?.makeKeyAndVisible()
         return true
--- XY.iOS/XY.iOS/resources/XY.iOS-Bridging-Header.h
+++ XY.iOS/XY.iOS/resources/XY.iOS-Bridging-Header.h
@@ -3,3 +3,4 @@
 //
 
 #import "ExampleViewController.h"
+#import "CameraController.h"
