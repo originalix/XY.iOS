0f52be79ec7dc955bf2c107939e98f0705f998f5
Lix <xiao.liunit@gmail.com>
2017-07-07 16:51:21 +0800

    add Face detect file

---
+++ XY.iOS/XY.iOS/resources/FaceDetect.h
@@ -0,0 +1,16 @@
+//
+//  FaceDetect.h
+//  XY.iOS
+//
+//  Created by Lix on 2017/7/7.
+//  Copyright © 2017年 Lix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface FaceDetect : NSObject
+
+- (void)faceDetectWithImage:(UIImage *)image;
+
+@end
---
+++ XY.iOS/XY.iOS/resources/FaceDetect.m
@@ -0,0 +1,17 @@
+//
+//  FaceDetect.m
+//  XY.iOS
+//
+//  Created by Lix on 2017/7/7.
+//  Copyright © 2017年 Lix. All rights reserved.
+//
+
+#import "FaceDetect.h"
+
+@implementation FaceDetect
+
+- (void)faceDetectWithImage:(UIImage *)image {
+    
+}
+
+@end
