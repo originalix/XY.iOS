b6072ca7ec13e8a80576b005550c0481a1cfbb77
Lix <xiao.liunit@gmail.com>
2017-07-10 14:36:09 +0800

    detect view controller

---
+++ XY.iOS/XY.iOS/Example/DetectViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetectViewController.h
+//  XY.iOS
+//
+//  Created by Lix on 2017/7/10.
+//  Copyright © 2017年 Lix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetectViewController : UIViewController
+
+@end
---
+++ XY.iOS/XY.iOS/Example/DetectViewController.m
@@ -0,0 +1,26 @@
+//
+//  DetectViewController.m
+//  XY.iOS
+//
+//  Created by Lix on 2017/7/10.
+//  Copyright © 2017年 Lix. All rights reserved.
+//
+
+#import "DetectViewController.h"
+
+@interface DetectViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
+
+@implementation DetectViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
