181f5aedfee1a77b7a79de70e1f7749f4617f7e4
Lix <xiao.liunit@gmail.com>
2017-07-20 14:12:50 +0800

    add webViewController

---
+++ XY.iOS/XY.iOS/Example/VueController.swift
@@ -0,0 +1,21 @@
+//
+//  VueController.swift
+//  XY.iOS
+//
+//  Created by Lix on 2017/7/20.
+//  Copyright © 2017年 Lix. All rights reserved.
+//
+
+import UIKit
+
+class VueController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
