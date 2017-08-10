ddc644a5cb8f9d4126a9205090e540421971f19d
Lix <xiao.liunit@gmail.com>
2017-08-10 14:26:09 +0800

    const and define

--- XY.iOS/XY.iOS/Example/EffictiveObjc.h
+++ XY.iOS/XY.iOS/Example/EffictiveObjc.h
@@ -8,6 +8,8 @@
 
 #import <Foundation/Foundation.h>
 
+extern NSString *const EFCStringConstant; //GOOD
+
 @interface EffictiveObjc : NSObject
 
 @end
--- XY.iOS/XY.iOS/Example/EffictiveObjc.m
+++ XY.iOS/XY.iOS/Example/EffictiveObjc.m
@@ -8,6 +8,26 @@
 
 #import "EffictiveObjc.h"
 
+#define ANIMATION_DURATION 0.3 //NOT GOOD
+
+static const NSTimeInterval kAnimationDuration = 0.3; //GOOD
+
+//if need be public const
+NSString *const EFCStringConstant = @"VALUE";
+
+@interface EffictiveObjc()
+
+@end
+
 @implementation EffictiveObjc
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
 
 @end
