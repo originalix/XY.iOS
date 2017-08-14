1e394b775585cdfc8acf4ded2b2be2e8decd16a3
Lix <xiao.liunit@gmail.com>
2017-08-14 12:25:10 +0800

    create menu controller

---
+++ XY.iOS/Pods/Target Support Files/LNTS_HttpClient/LNTS_HttpClient-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LNTS_HttpClient : NSObject
+@end
+@implementation PodsDummy_LNTS_HttpClient
+@end
---
+++ XY.iOS/XY.iOS/Example/MyFancyMenuViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MyFancyMenuViewController.swift
+//  XY.iOS
+//
+//  Created by Lix on 2017/8/14.
+//  Copyright © 2017年 Lix. All rights reserved.
+//
+
+import UIKit
+
+class MyFancyMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+}
