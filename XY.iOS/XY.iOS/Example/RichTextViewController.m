e8301408830b8aff5aa0d36669d7e452286e56fe
Lix <xiao.liunit@gmail.com>
2017-08-31 12:02:17 +0800

    test url

--- XY.iOS/XY.iOS/Example/RichTextViewController.m
+++ XY.iOS/XY.iOS/Example/RichTextViewController.m
@@ -8,7 +8,8 @@
 
 #import "RichTextViewController.h"
 
-@interface RichTextViewController ()
+@interface RichTextViewController () <UITextViewDelegate>
+
 @property (weak, nonatomic) IBOutlet UITextView *textView;
 
 @end
@@ -18,8 +19,18 @@
 #pragma mark - life cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.textView.text = @"#话题# Do you like wsx";
+    self.textView.text = @"#话题# hello world";
     [self checkRichText:self.textView.text];
+    self.textView.editable = false;
+    self.textView.selectable = true;
+//    //设置链接的属性 设置那一段颜色
+//    NSDictionary *linkAttributes =@{
+//                                    NSForegroundColorAttributeName:[UIColor redColor]};
+//    self.textView.linkTextAttributes = linkAttributes;
+//    /** 设置自动检测类型为链接网址. */
+//    self.textView.dataDetectorTypes = UIDataDetectorTypeAll;
+    
+    self.textView.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -28,21 +39,34 @@
 
 #pragma mark - Private Method
 - (void)checkRichText:(NSString *)text {
-//    NSString *text = @"#话题# Do you like wsx";
     NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:text];
     NSRange textRange = NSMakeRange(0, text.length);
     NSString *patterns = @"#[^#]+#";
     NSError *error = nil;
     NSRegularExpression *expression = [[NSRegularExpression alloc] initWithPattern:patterns options:NSRegularExpressionCaseInsensitive error:&error];
+    
     [expression enumerateMatchesInString:text options:NSMatchingReportProgress range:textRange usingBlock:^(NSTextCheckingResult * _Nullable result, NSMatchingFlags flags, BOOL * _Nonnull stop) {
-//        NSString *subString = [text substringWithRange:[result range]];
+        
         [attributedString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:30] range:[result range]];
         [attributedString addAttribute:NSForegroundColorAttributeName value:[UIColor blueColor] range:[result range]];
-//        self.textView.text = @"";
+        [attributedString addAttribute:NSLinkAttributeName value:@"topic:1" range:[result range]];
         self.textView.attributedText = attributedString;
-        
     }];
 }
 
+#pragma mark - textView delegate
+- (void)textViewDidBeginEditing:(UITextView *)textView {
+    NSLog(@"-> : %@", textView.text);
+}
+
+- (void)textViewDidChange:(UITextView *)textView {
+    NSLog(@"->> : %@", textView.text);
+    [self checkRichText:textView.text];
+}
+
+- (BOOL)textView:(UITextView *)textView shouldInteractWithURL:(NSURL *)URL inRange:(NSRange)characterRange {
+    NSLog(@"%@", URL.scheme);
+    return true;
+}
 
 @end
