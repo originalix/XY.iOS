85f0739749d842b9e902c4155641b1243deceef7
Lix <xiao.liunit@gmail.com>
2017-09-13 14:28:15 +0800

    create XYSearchViewController

---
+++ XY.iOS/XY.iOS/Sections/Wristband/M/XYSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYSearchViewController.h
+//  XY.iOS
+//
+//  Created by Lix on 2017/9/13.
+//  Copyright © 2017年 Lix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYSearchViewController : UIViewController
+
+@end
---
+++ XY.iOS/XY.iOS/Sections/Wristband/M/XYSearchViewController.m
@@ -0,0 +1,25 @@
+//
+//  XYSearchViewController.m
+//  XY.iOS
+//
+//  Created by Lix on 2017/9/13.
+//  Copyright © 2017年 Lix. All rights reserved.
+//
+
+#import "XYSearchViewController.h"
+
+@interface XYSearchViewController ()
+
+@end
+
+@implementation XYSearchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
