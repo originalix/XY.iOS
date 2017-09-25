a6d61858b844add3f790910d9e7325a37345ac8a
Lix <xiao.liunit@gmail.com>
2017-09-25 10:42:47 +0800

    坐标轴添加辅助线

--- XY.iOS/XY.iOS/Sections/Chart/C/XYXChartViewController.m
+++ XY.iOS/XY.iOS/Sections/Chart/C/XYXChartViewController.m
@@ -28,45 +28,48 @@
 #pragma mark - PNChart
 - (void)forLineChart {
     //For Line Chart
-    PNLineChart * lineChart = [[PNLineChart alloc] initWithFrame:CGRectMake(0, 135.0, SCREEN_WIDTH, 200.0)];
+    PNLineChart * lineChart = [[PNLineChart alloc] initWithFrame:CGRectMake(0, 135.0, SCREEN_WIDTH, 150)];
     lineChart.showGenYLabels = false;
-    lineChart.axisWidth = 10.f;
     lineChart.showSmoothLines = true;
-//    lineChart.showYGridLines = YES;
-//    lineChart.yGridLinesColor = [UIColor grayColor];
-//    lineChart.showLabel = YES;
-//    [lineChart setXLabels:@[@"SEP 1",@"SEP 2",@"SEP 3",@"SEP 4",@"SEP 5"]];
+
     [lineChart setXLabels:@[@"第21周",@"第22周",@"第23周",@"第24周",@"第25周"]];
     
     // Line Chart No.1
     NSArray * data01Array = @[@6666, @1600, @12640, @26220, @1862];
     PNLineChartData *data01 = [PNLineChartData new];
-    data01.inflexionPointStyle = PNLineChartPointStyleCircle;
+    data01.inflexionPointStyle = PNLineChartPointStyleNone;
     data01.inflexionPointColor = [UIColor redColor];
+    data01.inflexionPointWidth = 6.f;
+    data01.lineWidth = 4.f;
+    data01.alpha = 1.f;
     data01.color = PNFreshGreen;
     data01.itemCount = lineChart.xLabels.count;
-    data01.showPointLabel = true;
+//    data01.showPointLabel = true;
     data01.pointLabelFont = [UIFont systemFontOfSize:10.f];
     data01.pointLabelColor = [UIColor blackColor];
     data01.getData = ^(NSUInteger index) {
         CGFloat yValue = [data01Array[index] floatValue];
         return [PNLineChartDataItem dataItemWithY:yValue];
     };
-    // Line Chart No.2
-//    NSArray * data02Array = @[@20.1, @180.1, @26.4, @202.2, @126.2];
-//    PNLineChartData *data02 = [PNLineChartData new];
-//    data02.color = PNTwitterColor;
-//    data02.itemCount = lineChart.xLabels.count;
-//    data02.getData = ^(NSUInteger index) {
-//        CGFloat yValue = [data02Array[index] floatValue];
-//        return [PNLineChartDataItem dataItemWithY:yValue];
-//    };
-//
+
     lineChart.chartData = @[data01];
     lineChart.delegate = self;
     
     [lineChart strokeChart];
+//    lineChart.backgroundColor = [UIColor clearColor];
     [self.view addSubview:lineChart];
+    
+    NSLog(@"line1 = %@", lineChart.pathPoints);
+    NSMutableArray *array = lineChart.pathPoints;
+    for (NSArray *arr in array) {
+        for (int i = 0; i < arr.count; i++) {
+            CGPoint pt = [[arr objectAtIndex:i] CGPointValue];
+            NSLog(@"%f", pt.x);
+            UIView *line = [[UIView alloc] initWithFrame:CGRectMake(pt.x, 0, 1, lineChart.frame.size.height - 40)];
+            line.backgroundColor = [UIColor lightGrayColor];
+            [lineChart addSubview:line];
+        }
+    }
 }
 
 
