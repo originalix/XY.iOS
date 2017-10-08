cd51b40ced5602584b78b03f83b6827297b5808c
Lix <xiao.liunit@gmail.com>
2017-10-08 17:27:53 +0800

    no message

--- XY.iOS/XY.iOS/Sections/Chart/C/XYCalculateViewController.m
+++ XY.iOS/XY.iOS/Sections/Chart/C/XYCalculateViewController.m
@@ -13,6 +13,7 @@
 @property (nonatomic, strong) NSMutableString *firstStr;
 @property (nonatomic, strong) NSMutableString *secondStr;
 @property (nonatomic, assign) BOOL isFirst;
+@property (nonatomic, assign) BOOL isPlus;
 @end
 
 @implementation XYCalculateViewController
@@ -30,8 +31,14 @@
 }
 
 - (IBAction)showNumbers:(UIButton *)sender {
-    [self.firstStr appendString:[self realNumber:sender.tag]];
-    self.label.text = self.firstStr;
+    if (self.isFirst) {
+        [self.firstStr appendString:[self realNumber:sender.tag]];
+        self.label.text = self.firstStr;
+    } else {
+        [self.secondStr appendString:[self realNumber:sender.tag]];
+        self.label.text = self.secondStr;
+    }
+    
 }
 
 - (NSString *)realNumber:(NSInteger)tag {
@@ -39,11 +46,19 @@
 }
 
 - (IBAction)plusFunction:(id)sender {
-    
+    self.isFirst = false;
+    self.isPlus = true;
 }
 
 - (IBAction)resultFunction:(id)sender {
-    
+    NSInteger firstNum = [self.firstStr integerValue];
+    NSInteger secondNum = [self.secondStr integerValue];
+    if (self.isPlus) {
+        NSInteger result = firstNum + secondNum;
+        self.label.text = [NSString stringWithFormat:@"%ld", result];
+    }
+    self.firstStr = [NSMutableString string];
+    self.secondStr = [NSMutableString string];
 }
 
 @end
