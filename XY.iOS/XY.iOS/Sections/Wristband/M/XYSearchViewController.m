205315131cd37088843f2eea039aaed1f8ee1984
Lix <xiao.liunit@gmail.com>
2017-10-10 10:02:10 +0800

    search device for new sdk

--- XY.iOS/XY.iOS/Sections/Wristband/M/XYSearchViewController.m
+++ XY.iOS/XY.iOS/Sections/Wristband/M/XYSearchViewController.m
@@ -14,6 +14,9 @@
 @interface XYSearchViewController ()<LSScanDeviceDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) NSMutableArray *searchList;
+@property (nonatomic, assign) NSInteger filterRSSI;
+@property (nonatomic, copy) NSString *filterName;
 
 @end
 
@@ -21,7 +24,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    self.searchList = [NSMutableArray array];
+    self.filterRSSI = -100;
+    self.filterName = @"";
     [self.tableView registerNib:[UINib nibWithNibName:@"XYSearchDeviceCell" bundle:nil] forCellReuseIdentifier:@"cell"];
     
     [self startSearch];
@@ -32,12 +37,65 @@
 }
 
 - (void)startSearch {
+    [[LSDeviceManager shared] stopScan];
+    [self.searchList removeAllObjects];
+    [self.tableView reloadData];
     [[LSDeviceManager shared] scanDevice:self];
+}
+
+- (void)stopSearch {
+    [[LSDeviceManager shared] stopScan];
 }
 
 #pragma mark - LSScanDeviceDelegate
 - (void)onScanResult:(LSScanDeviceInfo *)deviceInfo {
     NSLog(@"deviceInfo = %@", [deviceInfo description]);
+    if (deviceInfo.rssi == 127) {
+        return;
+    }
+    
+    if (self.filterRSSI == 0 && self.filterName.length == 0) {
+        [self addDeviceToList:deviceInfo];
+        
+    } else if (self.filterRSSI == 0 && self.filterName.length > 0) {
+        if ([deviceInfo.name isEqualToString:self.filterName]) {
+            [self addDeviceToList:deviceInfo];
+        }
+        
+    } else if (self.filterRSSI != 0 && self.filterName.length == 0) {
+        if (deviceInfo.rssi >= self.filterRSSI) {
+            [self addDeviceToList:deviceInfo];
+        }
+        
+    } else if (self.filterRSSI != 0 && self.filterName.length > 0) {
+        if (deviceInfo.rssi >= self.filterRSSI && [deviceInfo.name isEqualToString:self.filterName]) {
+            [self addDeviceToList:deviceInfo];
+        }
+    }
+}
+
+- (void)addDeviceToList:(LSScanDeviceInfo *)deviceInfo {
+    for (LSEDevice *item in self.searchList) {
+        if ([item.deviceInfo.macAddress isEqualToString:deviceInfo.macAddress]) {
+            item.deviceInfo.rssi = deviceInfo.rssi;
+            return;
+        }
+    }
+    
+    LSEDevice *device = [[LSEDevice alloc] init];
+    device.deviceInfo = deviceInfo;
+    device.connectState = LSEDeviceStateDisConnect;
+    [self.searchList addObject:device];
+    
+    NSArray *result = [self.searchList sortedArrayUsingComparator:^NSComparisonResult(id _Nonnull obj1, id  _Nonnull obj2) {
+        LSEDevice *item1 = (LSEDevice *)obj1;
+        LSEDevice *item2 = (LSEDevice *)obj2;
+        return item1.deviceInfo.rssi < item2.deviceInfo.rssi;
+    }];
+    self.searchList = [NSMutableArray arrayWithArray:result];
+    
+    NSArray *position = @[[NSIndexPath indexPathForRow:[self.searchList indexOfObject:device] inSection:0]];
+    [self.tableView insertRowsAtIndexPaths:position withRowAnimation:UITableViewRowAnimationNone];
 }
 
 #pragma mark - Table view data source
@@ -47,7 +105,7 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 20;
+    return [self.searchList count];
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -56,17 +114,8 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
-//    XYSearchDeviceCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
-//    if (cell == nil) {
-//        cell = [[[NSBundle mainBundle] loadNibNamed:@"XYSearchDeviceCell" owner:self options:nil] firstObject];
-//    }
-    
     XYSearchDeviceCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
-    LSEDevice *device = [[LSEDevice alloc] init];
-    device.deviceInfo = [[LSScanDeviceInfo alloc] init];
-    device.deviceInfo.name = @"name!!!!";
-    device.deviceInfo.macAddress = @"12312312312";
-    device.deviceInfo.rssi = 2150;
+    LSEDevice *device = [self.searchList objectAtIndex:indexPath.row];
     [cell setContent:device];
     return cell;
 }
