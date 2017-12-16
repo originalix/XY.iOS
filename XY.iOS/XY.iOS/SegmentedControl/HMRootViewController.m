8eee7e347ae33cec6ff0558589b6f48576fbdec6
Lix <xiao.liunit@gmail.com>
2017-12-16 21:23:54 +0800

    create segment control

--- XY.iOS/XY.iOS/SegmentedControl/HMRootViewController.m
+++ XY.iOS/XY.iOS/SegmentedControl/HMRootViewController.m
@@ -9,6 +9,8 @@
 #import "HMRootViewController.h"
 #import "HMSegmentedControl.h"
 
+#define kScreenWidth [UIScreen mainScreen].bounds.size.width
+
 @interface HMRootViewController ()
 
 @end
@@ -17,10 +19,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self setupUIView];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+}
+
+- (void)setupUIView {
+    HMSegmentedControl *segmentedControl = [[HMSegmentedControl alloc] initWithSectionTitles:@[@"One", @"Two", @"Three"]];
+    segmentedControl.frame = CGRectMake(0, 0, kScreenWidth, 60);
+    [segmentedControl addTarget:self action:@selector(segmentedControlChangedValue:) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:segmentedControl];
+}
+
+- (void)segmentedControlChangedValue:(NSInteger)index {
+    NSLog(@"%ld", index);
 }
 
 @end
