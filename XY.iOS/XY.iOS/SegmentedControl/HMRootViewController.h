dfa97bf56e042d884938660ecb106225ec33da67
Lix <xiao.liunit@gmail.com>
2017-12-16 21:19:44 +0800

    create HMSegmentController

---
+++ XY.iOS/XY.iOS/SegmentedControl/HMRootViewController.h
@@ -0,0 +1,13 @@
+//
+//  HMRootViewController.h
+//  XY.iOS
+//
+//  Created by Lix on 2017/12/16.
+//  Copyright © 2017年 Lix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HMRootViewController : UIViewController
+
+@end
---
+++ XY.iOS/XY.iOS/SegmentedControl/HMRootViewController.m
@@ -0,0 +1,26 @@
+//
+//  HMRootViewController.m
+//  XY.iOS
+//
+//  Created by Lix on 2017/12/16.
+//  Copyright © 2017年 Lix. All rights reserved.
+//
+
+#import "HMRootViewController.h"
+#import "HMSegmentedControl.h"
+
+@interface HMRootViewController ()
+
+@end
+
+@implementation HMRootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
