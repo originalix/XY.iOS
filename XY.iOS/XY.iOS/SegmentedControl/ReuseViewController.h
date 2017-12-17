d38bf38447a203d21c8d3d88266b9343d5fec962
Lix <xiao.liunit@gmail.com>
2017-12-17 08:36:44 +0800

    create reuse controller

---
+++ XY.iOS/XY.iOS/SegmentedControl/ReuseViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReuseViewController.h
+//  XY.iOS
+//
+//  Created by Lix on 2017/12/17.
+//  Copyright © 2017年 Lix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReuseViewController : UIViewController
+
+@end
---
+++ XY.iOS/XY.iOS/SegmentedControl/ReuseViewController.m
@@ -0,0 +1,25 @@
+//
+//  ReuseViewController.m
+//  XY.iOS
+//
+//  Created by Lix on 2017/12/17.
+//  Copyright © 2017年 Lix. All rights reserved.
+//
+
+#import "ReuseViewController.h"
+
+@interface ReuseViewController ()
+
+@end
+
+@implementation ReuseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
