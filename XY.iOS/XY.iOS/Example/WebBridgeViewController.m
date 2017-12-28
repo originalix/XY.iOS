9ee426b42c2ccb8af17803a75fa5ed06cceefb41
Lix <xiao.liunit@gmail.com>
2017-12-28 11:28:16 +0800

    web view js bridge

--- XY.iOS/XY.iOS/Example/WebBridgeViewController.m
+++ XY.iOS/XY.iOS/Example/WebBridgeViewController.m
@@ -21,10 +21,32 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self initWithWebView];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+}
+
+- (void)initWithWebView {
+    WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
+    WKUserContentController *userController = [[WKUserContentController alloc] init];
+    config.userContentController = userController;
+    self.webView = [[WKWebView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height) configuration:config];
+    [self.view addSubview:self.webView];
+    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.baidu.com"]]];
+}
+
+- (void)initBridge {
+    self.bridge = [WebViewJavascriptBridge bridgeForWebView:_webView];
+    [self.bridge registerHandler:@"ObjC Echo" handler:^(id data, WVJBResponseCallback responseCallback) {
+        NSLog(@"ObjC Echo called with: %@", data);
+        responseCallback(data);
+    }];
+    
+    [self.bridge callHandler:@"JS Echo" data:nil responseCallback:^(id responseData) {
+        NSLog(@"ObjC received response: %@", responseData);
+    }];
 }
 
 @end
