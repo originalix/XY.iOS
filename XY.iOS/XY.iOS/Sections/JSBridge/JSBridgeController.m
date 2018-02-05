8cd3c8fe0b67638e7ed04b6782c66d0f936c6faa
Lix <xiao.liunit@gmail.com>
2018-02-05 13:40:12 +0800

    [add jsbridge controller]

---
+++ XY.iOS/XY.iOS/Sections/JSBridge/JSBridgeController.m
@@ -0,0 +1,37 @@
+//
+//  JSBridgeController.m
+//  XY.iOS
+//
+//  Created by Lix on 2018/2/5.
+//  Copyright © 2018年 Lix. All rights reserved.
+//
+
+#import "JSBridgeController.h"
+
+@interface JSBridgeController ()
+
+@end
+
+@implementation JSBridgeController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
