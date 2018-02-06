6d650cc423e843a164da3e042081a2b191cbf3c3
Lix <xiao.liunit@gmail.com>
2018-02-06 13:40:29 +0800

    [完成htmlcallapp 对接]

--- XY.iOS/XY.iOS/Sections/JSBridge/JSBridgeController.m
+++ XY.iOS/XY.iOS/Sections/JSBridge/JSBridgeController.m
@@ -7,9 +7,10 @@
 //
 
 #import "JSBridgeController.h"
-#import <JavaScriptCore/JavaScriptCore.h>
+//#import <JavaScriptCore/JavaScriptCore.h>
+#import "UIWebView+TS_JavaScriptContext.h"
 
-@interface JSBridgeController ()<UIWebViewDelegate>
+@interface JSBridgeController ()<TSWebViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
 
@@ -20,7 +21,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://192.168.0.110/code-repo/bridge.html"]]];
-    self.webView.delegate = self;
+//    self.webView.delegate = self;
     UIBarButtonItem *item = [[UIBarButtonItem alloc] initWithTitle:@"refresh" style:UIBarButtonItemStylePlain target:self action:@selector(refresh)];
     self.navigationItem.leftBarButtonItem = item;
 }
@@ -33,12 +34,26 @@
     [super didReceiveMemoryWarning];
 }
 
-
-- (void)webViewDidStartLoad:(UIWebView *)webView {
-}
-
-
-- (void)webViewDidFinishLoad:(UIWebView *)webView {
+- (void)webView:(UIWebView *)webView didCreateJavaScriptContext:(JSContext *)ctx {
+    ctx[@"htmlCallApp"] = ^NSDictionary *(NSString *eventName, NSString *params) {
+        if ([eventName isEqualToString:@"getUserAgent"]) {
+            return @{
+                     @"app_token" : @"lixxxxxx",
+                     @"user_id" : @2150,
+                     @"platform_id" : @2,
+                     };
+        } else if ([eventName isEqualToString:@"login"]) {
+            NSLog(@"调起登录, 包含参数: %@", params);
+            return nil;
+        } else if ([eventName isEqualToString:@"pay"]) {
+            NSLog(@"弹起支付, 包含参数: %@", params);
+            return @{
+                     @"orderInfo": @"order"
+                     };
+        }
+        
+        return nil;
+    };
 }
 
 @end
