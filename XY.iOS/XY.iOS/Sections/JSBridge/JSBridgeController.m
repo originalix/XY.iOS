fe93b2f6d1e9dd9e23acb56e500724a703ad3b53
Lix <xiao.liunit@gmail.com>
2018-02-06 10:10:53 +0800

    [test bridge]

--- XY.iOS/XY.iOS/Sections/JSBridge/JSBridgeController.m
+++ XY.iOS/XY.iOS/Sections/JSBridge/JSBridgeController.m
@@ -32,6 +32,12 @@ JSExportAs
     [super viewDidLoad];
     [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://192.168.0.110/code-repo/bridge.html"]]];
     self.webView.delegate = self;
+    UIBarButtonItem *item = [[UIBarButtonItem alloc] initWithTitle:@"refresh" style:UIBarButtonItemStylePlain target:self action:@selector(refresh)];
+    self.navigationItem.leftBarButtonItem = item;
+}
+
+- (void)refresh {
+    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://192.168.0.110/code-repo/bridge.html"]]];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -52,42 +58,31 @@ JSExportAs
         context.exception = exceptionValue;
     };
     
+    NSLog(@"context start load: %@", self.context);
+    
     __weak typeof(self)temp = self;
-    //    self.context[@"myAction"] = ^(){
-    //
-    //        [temp.navigationController popViewControllerAnimated:YES];
-    //
-    //    };
-    //    self.context[@"log"] = ^(NSString *string){
-    //
-    //        NSLog(@"%@",string);
-    //
-    //    };
-    //
-    //    self.context[@"native"] = self;
     
-    //    NSString* (^logBlock)(NSString *,NSString *) = ^(NSString * str1,NSString *str2){
-    //        return [NSString stringWithFormat:@"%@%@",str1,str2];
-    //    };
-    //
-    //    self.context[@"htmlCallApp"] =  ^(NSString *a, NSString *b) {
-    //        logBlock(a, b);
-    //    };
-    
-    //    self.context[@"htmlCallApp"]  = ^JSValue *(JSValue *string, JSValue *a){
-    //        NSLog(@"%@", [string toString]);
-    //        return [JSValue valueWithObject:@"asdfasdfad" inContext:temp.context];
-    //    };
-    
-    self.context[@"ocAlert"] = ^JSValue *(JSValue *string){
+    self.context[@"htmlCallApp"]  = ^JSValue *(JSValue *string, JSValue *a){
         NSLog(@"%@", [string toString]);
-        return [JSValue valueWithObject:@"oc ahh" inContext:temp.context];
+        return [JSValue valueWithObject:@"appTokennnnnnn" inContext:temp.context];
     };
     
     self.context[@"jsCallApp"]  = ^JSValue *(JSValue *string, JSValue *a){
         NSLog(@"%@", [string toString]);
-        return [JSValue valueWithObject:@"apptokennnnnnn" inContext:temp.context];
+        return [JSValue valueWithObject:@"apptokennnnnnn start load" inContext:temp.context];
     };
+    
+    
+//
+//    self.context[@"ocAlert"] = ^JSValue *(JSValue *string){
+//        NSLog(@"%@", [string toString]);
+//        return [JSValue valueWithObject:@"oc ahh" inContext:temp.context];
+//    };
+//
+//    self.context[@"jsCallApp"]  = ^JSValue *(JSValue *string, JSValue *a){
+//        NSLog(@"%@", [string toString]);
+//        return [JSValue valueWithObject:@"apptokennnnnnn" inContext:temp.context];
+//    };
 }
 
 
@@ -102,6 +97,8 @@ JSExportAs
     };
     
     __weak typeof(self)temp = self;
+    
+    NSLog(@"context did finish : %@", self.context);
     //    self.context[@"myAction"] = ^(){
     //
     //        [temp.navigationController popViewControllerAnimated:YES];
@@ -125,17 +122,17 @@ JSExportAs
     
     self.context[@"htmlCallApp"]  = ^JSValue *(JSValue *string, JSValue *a){
         NSLog(@"%@", [string toString]);
-        return [JSValue valueWithObject:@"asdfasdfad" inContext:temp.context];
+        return [JSValue valueWithObject:@"tokennnnn" inContext:temp.context];
     };
-    
-    self.context[@"ocAlert"] = ^JSValue *(JSValue *string){
-        NSLog(@"%@", [string toString]);
-        return [JSValue valueWithObject:@"oc ahh" inContext:temp.context];
-    };
-    
+
+//    self.context[@"ocAlert"] = ^JSValue *(JSValue *string){
+//        NSLog(@"%@", [string toString]);
+//        return [JSValue valueWithObject:@"oc ahh" inContext:temp.context];
+//    };
+//
     self.context[@"jsCallApp"]  = ^JSValue *(JSValue *string, JSValue *a){
         NSLog(@"%@", [string toString]);
-        return [JSValue valueWithObject:@"apptokennnnnnn" inContext:temp.context];
+        return [JSValue valueWithObject:@"apptokennnnnnn did finish" inContext:temp.context];
     };
 }
 
