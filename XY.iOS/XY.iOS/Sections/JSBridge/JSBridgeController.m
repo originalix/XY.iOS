e14e5356fe933deb8f617fa10320cc2758671de8
Lix <xiao.liunit@gmail.com>
2018-02-06 13:47:17 +0800

    [add delegate for webView]

--- XY.iOS/XY.iOS/Sections/JSBridge/JSBridgeController.m
+++ XY.iOS/XY.iOS/Sections/JSBridge/JSBridgeController.m
@@ -21,7 +21,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://192.168.0.110/code-repo/bridge.html"]]];
-//    self.webView.delegate = self;
+    self.webView.delegate = self;
     UIBarButtonItem *item = [[UIBarButtonItem alloc] initWithTitle:@"refresh" style:UIBarButtonItemStylePlain target:self action:@selector(refresh)];
     self.navigationItem.leftBarButtonItem = item;
 }
